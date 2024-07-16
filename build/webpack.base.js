07871f7450d20a96164ad0a0291db6168d592473
yekang <wb-yekang@rokid.com>
2024-07-16 18:40:05 +0800

    feat: webpack包分析工具

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -32,7 +32,7 @@ module.exports = {
       {
         // 从右往左,从下往上的,遇到less文件,使用less-loader解析为css
         // 匹配到css文件后先用css-loader解析css, 最后借助style-loader把css插入到头部style标签中
-        test: /.css/, //匹配 css和less 文件
+        test: /.css$/, //匹配 css和less 文件
         use: [
           "style-loader",
           "css-loader",
@@ -51,7 +51,7 @@ module.exports = {
       {
         // 从右往左,从下往上的,遇到less文件,使用less-loader解析为css
         // 匹配到css文件后先用css-loader解析css, 最后借助style-loader把css插入到头部style标签中
-        test: /.less/, //匹配 css和less 文件
+        test: /.less$/, //匹配 css和less 文件
         use: [
           "style-loader",
           "css-loader",
