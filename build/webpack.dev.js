22bbc6e3791b7d0561d52a09cd3595b191b7136e
yekang <wb-yekang@rokid.com>
2024-07-16 18:31:18 +0800

    fix: 添加源码错误映射

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -13,6 +13,9 @@ const baseConfig = require("./webpack.base.js");
 // 合并公共配置,并添加开发环境配置
 module.exports = merge(baseConfig, {
   mode: "development", // 开发模式,打包更加快速,省了代码优化步骤
+  // 本地开发首次打包慢点没关系,因为 eval 缓存的原因, 热更新会很快
+  // 开发中,我们每行代码不会写的太长,只需要定位到行就行,所以加上 cheap
+  // 我们希望能够找到源代码的错误,而不是打包后的,所以需要加上 module
   devtool: "eval-cheap-module-source-map", // 源码调试模式,后面会讲
   devServer: {
     port: 8000, // 服务端口号
